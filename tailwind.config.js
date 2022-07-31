15a4d9dd983693d8f65895682ecd60811687dd2e
Shiv Bhonde <shivbhonde04@gmail.com>
2022-07-31 21:59:26 +0530

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			keyframes: {
+				'fade-in-down': {
+					'0%': {
+						opacity: '0',
+						transform: 'translateY(-10px)',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translateY(0)',
+					},
+				},
+				'fade-out-up': {
+					from: {
+						opacity: '1',
+						transform: 'translateY(0px)',
+					},
+					to: {
+						opacity: '0',
+						transform: 'translateY(10px)',
+					},
+				},
+			},
+			animation: {
+				'fade-in-down': 'fade-in-down 0.5s ease-out',
+				'fade-out-up': 'fade-out-up 0.5s ease-out',
+			},
+			screens: {
+				mf: '950px',
+			},
+		},
+	},
+	plugins: [],
+};
